f97c372e73f82f6d50b426dcd810eb521fec0188
Alexei Ledenev <alexei.led@gmail.com>
2018-09-09 12:30:49 +0300

    enrich codebuild env

--- hack/codebuild_build.sh
+++ hack/codebuild_build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 CODEBUILD_GIT_BRANCH="$(git symbolic-ref HEAD --short 2>/dev/null)"
-if [ "$CODEBUILD_GIT_BRANCH" = "" ] ; then
+if [ "$CODEBUILD_GIT_BRANCH" = "" ]; then
   CODEBUILD_GIT_BRANCH="$(git branch -a --contains HEAD | sed -n 2p | awk '{ printf $1 }')";
-  export CODEBUILD_GIT_BRANCH=${CODEBUILD_GIT_BRANCH#remotes/origin/};
+  CODEBUILD_GIT_BRANCH=${CODEBUILD_GIT_BRANCH#remotes/origin/};
 fi
 
 readonly account=$(aws sts get-caller-identity --query 'Account' --output text)
