5f755ea502201350b768df1670fe11b141a040fa
Alexei Ledenev <alexei.led@gmail.com>
2018-09-09 09:18:19 +0300

    fix tags

--- hack/codebuild_build.sh
+++ hack/codebuild_build.sh
@@ -6,30 +6,30 @@ readonly commit=$(git rev-parse --short HEAD 2>/dev/null)
 readonly version=$(cat VERSION)
 
 # Attempt to pull existing builder image
-if docker pull ${repo}:builder-branch; then
+if docker pull ${repo}:builder-${branch}; then
     # Update builder image
-    docker build -t ${repo}:builder-branch --target builder --cache-from ${repo}:builder-branch -f docker/Dockerfile .
+    docker build -t ${repo}:builder-${branch} --target builder --cache-from ${repo}:builder-${branch} -f docker/Dockerfile .
 else
     # Create new builder image
-    docker build -t ${repo}:builder-branch --target builder -f docker/Dockerfile .
+    docker build -t ${repo}:builder-${branch} --target builder -f docker/Dockerfile .
 fi
 
 # Attempt to pull latest branch target image
-docker pull ${repo}:branch || true
+docker pull ${repo}:${branch} || true
 
 # Build and push target image
-docker build -t ${repo}:branch --cache-from ${repo}:builder-branch --cache-from ${repo}:branch \
+docker build -t ${repo}:branch --cache-from ${repo}:builder-${branch} --cache-from ${repo}:${branch} \
   --build-arg GH_SHA=${commit} \
   --build-arg GITHUB_TOKEN=$GITHUB_TOKEN \
   --build-arg CODECOV_TOKEN=$CODECOV_TOKEN \
   -f docker/Dockerfile .
 
-docker push ${repo}:branch
+docker push ${repo}:${branch}
 
 # for master push versioned image too
-if [ "${version}" == "master" ]; then
-    docker push ${repo}:version
+if [ "${branch}" == "master" ]; then
+    docker push ${repo}:${version}
 fi
 
 # Push builder image
-docker push ${repo}:builder-branch
+docker push ${repo}:builder-${branch}
